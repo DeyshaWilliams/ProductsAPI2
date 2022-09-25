1c4f69d8e04d7658a62568204bb3a41443aff3ce
Deysha Williams <deyshawilliams95@gmail.com>
2022-09-24 19:16:51 -0700

    Changed models and serializers list to match project

--- Products/models.py
+++ Products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    brand = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    year = models.IntegerField()
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
+    
