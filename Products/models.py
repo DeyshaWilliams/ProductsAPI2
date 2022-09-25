bc6e50c52738478422ef6e36f99a10ab7833526d
Deysha Williams <deyshawilliams95@gmail.com>
2022-09-24 19:30:03 -0700

    Tried to change lists, but it didn't work, so, it won't match

--- Products/models.py
+++ Products/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    year = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    inventory_quantity = models.IntegerField()
-    
